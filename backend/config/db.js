cc257ea03346d922788fdf07655babd642ff2219
shirish-raj-gupta <codersrg@gmail.com>
2025-06-08 13:02:04 +0530

    setting up env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-   await mongoose.connect('mongodb+srv://foodDelivery:food_Delivery01@cluster0.89zyueu.mongodb.net/food-del').then(()=>console.log('db connected'));
+   await mongoose.connect(`${process.env.MONGODB_URI}/food-del`).then(()=>console.log('db connected'));
 
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express"
 import cors from "cors"
+import "dotenv/config"
 import {connectDB} from './config/db.js'
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 const app = express()
 const port = 4000
@@ -16,6 +18,7 @@ connectDB();
 //api endpoints
 app.use('/api/food',foodRouter)
 app.use('/images',express.static('uploads'))
+app.use('/api/user', userRouter)
 
 app.get("/", (req,res)=>{
   res.send("Hello")
